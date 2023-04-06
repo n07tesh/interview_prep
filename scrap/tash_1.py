4ea3fd7bdd3950698c989955ec1d5111b72d53e1
Nitesh Yadav <niteshyadav071@gmail.com>
2023-04-06 13:54:29 +0530

    prep

--- scrap/tash_1.py
+++ scrap/tash_1.py
@@ -4,15 +4,16 @@ from selenium.webdriver.common.by import By
 import time
 from bs4 import BeautifulSoup
 # import pandas as pd
-
-n = 1
+import csv
 class Xpath:
     url = 'https://mathup.com/games/crossbit'
     play_button = '/html/body/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[3]/div[1]'
     web_driver_loc = r'D:\web_driver\chromedriver.exe'
     div_class = 'GamePostStart_info__Rwi7G'
-while n <=10:
-    def web_driver():
+    file_name = 'scrap_data.csv'
+    fields = ['S.no','Difficulty']    
+
+    def web_driver(self,):
         try:
             options = Options()
             options.add_argument('--incognito')
@@ -23,14 +24,30 @@ while n <=10:
             time.sleep(2)
             driver.find_element(By.XPATH,Xpath.play_button).click()
             time.sleep(3)
-            page_source_ = driver.page_source
-            soup = BeautifulSoup(page_source_,'html.parser')
-            div_ = soup.find_all('div')[0:1]
-            next_ = div_.find_all('div')[0]
-            print(next_)
+            rows = []
+            with open(Xpath.file_name,'w') as csvfile:
+                # writing the fields 
+                csv_writer = csv.writer(csvfile)
+                csv_writer.writerow(Xpath.fields)
+                for i in range(10):
+                    page_source_ = driver.page_source
+                    soup = BeautifulSoup(page_source_,'html.parser')
+                    time.sleep(1)
+                    x = soup.find_all('div',{'class':'GamePostStart_info__Rwi7G'})[1].text.split(" ")[1]
+                    temp = []
+                    temp.append(i)
+                    temp.append(x)
+                    rows.append(temp)
+                    # writing the data rows 
+                    csv_writer.writerows(rows)
+                    driver.refresh()
+                    driver.find_element(By.XPATH,Xpath.play_button).click()
+                    time.sleep(3)
+                    continue
 
         except Exception as e:
             driver.close()
 
 if __name__ == "__main__":
-    web_driver()
+    x = Xpath()
+    x.web_driver()
